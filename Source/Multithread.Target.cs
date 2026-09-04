984724d6945aa46e62de82954de2b9edf6a60b8e
Leandro Peres <leandroperes@protonmail.com>
2026-09-04 11:46:35 -0300

    chore(build): wire DeferredJobs module, move to UE 5.8
    Targets add DeferredJobs module. Editor target V6->V7, include order 5_7->5_8. uproject engine association update plus DeferredJobs module entry. DefaultGame.ini AssetManager quoting fix and GameFeatureData AlwaysCook.

--- Source/Multithread.Target.cs
+++ Source/Multithread.Target.cs
@@ -11,5 +11,6 @@ public class MultithreadTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Multithread");
+		ExtraModuleNames.Add("DeferredJobs");
 	}
 }
--- Source/MultithreadEditor.Target.cs
+++ Source/MultithreadEditor.Target.cs
@@ -8,8 +8,9 @@ public class MultithreadEditorTarget : TargetRules
 	public MultithreadEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("Multithread");
+		ExtraModuleNames.Add("DeferredJobs");
 	}
 }
